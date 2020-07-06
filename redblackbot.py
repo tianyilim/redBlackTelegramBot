a74336c7336495b1071516512eb8d7f4ff32a08a
tianyilim <0.tianyi.lim@gmail.com>
2020-07-06 22:40:29 +0800

    Initial Commit

---
+++ redblackbot.py
@@ -0,0 +1,15 @@
+import telegram
+# from telebot.credentials import bot_token, bot_username, URL
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
+
+bot_token = "1167359926:AAEl5xxn1enjFpmygGD7wEY9M0hQvU4uR-M"
+bot_username = "t.me/RedBlack_Wholesome_Bot"
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
